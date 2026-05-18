035520eafd2fef5fcc9a14aced278cd14688a1da
Jesse Hamwijk <jlhamwijk@gmail.com>
2026-05-18 23:33:45 +0200

    Working console version, with add, show, delete and complete functions.

--- Program.cs
+++ Program.cs
@@ -2,25 +2,42 @@
 
 bool isRunning = true;
 TaskManager taskManager = new TaskManager();
-
+Console.WriteLine("Welcome to the Todo App");
 
 while (isRunning)
-{
-    Console.WriteLine("Welcome to the Todo App");
+{   
+    Console.WriteLine("What do you want to do now?");
     Console.WriteLine("choose an option and enter a number:");
     Console.WriteLine("1. Add a task");
     Console.WriteLine("2. Show tasks");
     Console.WriteLine("3. Complete task");
     Console.WriteLine("4. Delete task");
     Console.WriteLine("5. Exit");
-    
     string commandinput = Console.ReadLine();
-    string addinput = Console.ReadLine();
+
     switch (commandinput)
     {
         case "1":
+            Console.WriteLine("Enter the task title:");
+            string addinput = Console.ReadLine();
             taskManager.AddTask(addinput);
             break;
-
+        case "2":
+            taskManager.ShowTasks();
+            break;
+        case "3":
+            Console.WriteLine("Enter the task title:");
+            string completeinput = Console.ReadLine();
+            taskManager.CompleteTask(completeinput);
+            break;
+        case "4":
+            Console.WriteLine("Enter the task title:");
+            string deleteinput = Console.ReadLine();
+            taskManager.DeleteTask(deleteinput);
+            break;
+        case "5":
+            isRunning = false;
+            break;
     }
+    
 }
--- TaskManager.cs
+++ TaskManager.cs
@@ -2,10 +2,42 @@ using System.Runtime.CompilerServices;
 
 class TaskManager
 {
-    private List<TodoTask> tasks;
+    private List<TodoTask> tasks = new List<TodoTask>();
 
     public void AddTask(string title)
     {
         tasks.Add(new TodoTask { Title = title, IsCompleted = false });
+    }
+
+    public void ShowTasks()
+    {
+        foreach (var task in tasks)
+        {
+            Console.WriteLine($"{task.Title} - {(task.IsCompleted ? "Completed" :"Not Completed")}");
+        }
+    }
+
+    public void CompleteTask(string title)
+    {
+        foreach (var task in tasks)
+        {
+            if (task.Title == title)
+            {
+                task.IsCompleted = true;
+                break;
+            }
+        }
+    }
+
+    public void DeleteTask(string title)
+    {
+        foreach (var task in tasks)
+        {
+            if (task.Title == title)
+            {
+                tasks.Remove(task);
+                break;
+            }        
+        }
     }
 }
